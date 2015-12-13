b005f328943adc0632ef2f0c4b8b0566b3cf3fbd
sandeep3004 <sandeep.bansal@hpe.com>
2015-12-13 17:58:36 +0530

    Update AppTest.java
    fasf

--- dateUtils/src/test/java/com/mkyong/core/utils/AppTest.java
+++ dateUtils/src/test/java/com/mkyong/core/utils/AppTest.java
@@ -5,9 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
- */
-public class AppTest 
+ * Unit test for simple App. asfasdfasf
     extends TestCase wedfwefe
 {
     /**
